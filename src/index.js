7fc10517c6868b6a26bea863469266b7a2248d88
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-21 12:36:02 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector("#search-box");
+
+searchBox.addEventListener("input", () => {
+    
+})
+function fetchCountries(name) {
+    return fetch("https://restcountries.com/v3.1/name/{name}").then(
+          (response) => {
+            if (!response.ok) {
+              throw new Error(response.status);
+            }
+            return response.json();
+          }
+        );
+      }
