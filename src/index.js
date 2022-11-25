21642d5385b05a9e8269906fcc5631d61bcc2405
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-25 21:53:33 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list")
+const countryInfo = document.querySelector(".country-info")
 
 searchBox.addEventListener("input", () => {
-    
+    fetchCountries(name).then((country) => renderCountryList(countries)).catch((error) => console.log(error));
+
 })
+
 function fetchCountries(name) {
-    return fetch("https://restcountries.com/v3.1/name/{name}").then(
+    return fetch("https://restcountries.com/v2/{service}?fields={field},{field},{field}").then(
           (response) => {
             if (!response.ok) {
               throw new Error(response.status);
@@ -16,4 +20,12 @@ function fetchCountries(name) {
             return response.json();
           }
         );
-      }
+}
+
+
+function renderCountryList(countries) {
+    const markup = countries.map((country) => {
+        return `<li><p>${country.name.official}</p></li>`
+    }).join("");
+    countryList.innerHTML = markup;
+}
